78efb5c18da82ccdbbb5793d602241e0d2c9336c
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-01-11 00:22:07 +0300

    all tasks complete

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -9,7 +10,8 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         // TASK: Add one sentence to save the project to the logged-in user
-        //   by $request->project_id and with $request->start_date parameter
+        //   by  and with $request->start_date parameter
+        auth()->user()->projects()->attach($request->project_id, ['start_date' => $request->start_date]);
 
         return 'Success';
     }
--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -14,6 +14,6 @@ class Attachment extends Model
     public function attachable()
     {
         // TASK: fill in the code to make it work
-        return $this->morphedByMany([Task::class, Comment::class], 'attachable');
+        return $this->morphTo();
     }
 }
